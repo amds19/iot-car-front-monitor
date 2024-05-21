697a82662a0f22f5da71f1795d4235e859c305ca
amds19 <martinuser029@hotmail.com>
2024-05-21 12:45:15 -0600

    Add files via upload

---
+++ monitor/js/main.js
@@ -0,0 +1,40 @@
+let respuesta = document.getElementById("respuesta");
+
+function callApiRequest() {
+  // Hacer una petición para un usuario con ID especifico
+  axios
+    .get("http://18.169.240.40/iot-car-control/back-end/apis/getRegistro.php")
+    
+    .then(function (response) {
+      // manejar respuesta exitosa
+      console.log(response);
+      switch (response.data){
+        case "f":
+          respuestaServidor = "ADELANTE"; break;
+        case "b":
+          respuestaServidor = "ATRÁS"; break;
+        case "l":
+          respuestaServidor = "IZQUIERDA"; break;
+        case "r":
+          respuestaServidor = "DERECHA"; break;
+        default:
+          respuestaServidor = "DETENER"; break;
+      }
+
+      // let respuestaServidor = response.data == "f"? "ADELANTE"
+      //                       : response.data == "b"? "ATRÁS"
+      //                       : response.data == "l"? "IZQUIERDA"
+      //                       : responde.data == "r"? "DERECHA"
+      //                       : "DETENER";
+      respuesta.innerHTML="Respuesta: <strong>" + respuestaServidor + "</strong>";
+    })
+    .catch(function (error) {
+      // manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // siempre sera executado
+    });
+}
+
+setInterval(callApiRequest, 2000);
